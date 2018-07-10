a86cffbf5679430218cc73e2a77ba3cf7e027c62
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-10 18:11:29 +0200

    update to be compatible with user admin

--- includes/util/login-or-create-user.php
+++ includes/util/login-or-create-user.php
@@ -1,30 +1,55 @@
 <?php
 
 if ( !function_exists( 'magic_login_or_create_user' ) ) {
-  function magic_login_or_create_user() {
+  function magic_login_or_create_user( array $ctx ) {
+    $arguments = array (
+      'log' => 'missing_log',
+      'pwd' => 'missing_pwd',
+      'pwd2' => false,
+    );
+
     $user = wp_get_current_user();
 
     if ( empty( $user->ID ) ) {
-      if ( !username_exists( $_POST['email'] ) && !email_exists($_POST['email']) ) {
-        if ( $_POST['password'] !== $_POST['password2'] ) {
-          magic_redirect( [ 'error' => 'password_mismatch' ] );
+      if ( !username_exists( $_POST['log'] ) && !email_exists($_POST['log']) ) {
+        $arguments['pwd2'] = 'missing_pwd2';
+
+        $ctx = magic_parse_arguments( $arguments, $ctx );
+
+        if ( !empty( $ctx['errors'] ) ) {
+          return $ctx;
         }
-      	$user->ID = wp_create_user( $_POST['email'], $_POST['password'], $_POST['email'] );
+
+        if ( $ctx['query']['pwd'] !== $ctx['query']['pwd2'] ) {
+          $ctx['errors'][] = 'mismatch_password';
+        }
+
+        if ( !empty( $ctx['errors'] ) ) {
+          return $ctx;
+        }
+
+      	$created_user_id = wp_create_user( $ctx['query']['log'], $ctx['query']['pwd'], $ctx['query']['log'] );
+
+        if ( is_wp_error( $created_user_id ) || empty( $created_user_id ) ) {
+          $ctx['errors'][] = 'create_user';
+        }
       }
 
-      $credentials = array(
-        'user_login' => $_POST['email'],
-        'user_password' => $_POST['password'],
-        'remember' => isset($_POST['remember']) ? $_POST['remember'] : false,
-      );
+      $ctx = magic_parse_arguments( $arguments, $ctx );
 
-      $user = wp_signon( $credentials );
+      if ( !empty( $ctx['errors'] ) ) {
+        return $ctx;
+      }
+
+      $user = wp_signon();
+
+      if ( is_wp_error( $user ) || empty( $user ) ) {
+        $ctx['errors'][] = 'signon';
+      } else {
+        $ctx['user'] = $user;
+      }
     }
 
-    if ( is_wp_error( $user ) || empty( $user->ID) ) {
-      magic_redirect( [ 'error' => 'signon' ] );
-    }
-
-    return $user;
+    return $ctx;
   }
 }
