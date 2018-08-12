89f2b2edbaa14610d6b615111d9b6bf739bafcc1
Jascha Ehrenreich <jascha@jaeh.at>
2018-08-12 17:23:28 +0200

    add requirements, add index.php for admin, make plugin not fail if timber is missing

---
+++ includes/admin/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'dashboard.php';
+require_once 'plugin-activation.php';
+require_once 'requirements.php';
--- includes/plugin.php
+++ includes/plugin.php
@@ -1,24 +1,27 @@
 <?php
 
+if ( is_admin() ) {
+  require_once plugin_dir_path( __FILE__ ) . 'admin/index.php';
+}
+
 require_once 'util/options.php';
 require_once 'util/cookies.php';
 require_once 'util/slugify.php';
 require_once 'util/styles.php';
 
 require_once 'util/request-handler.php';
+// require_once 'util/magic-db.php';
 require_once 'util/custom-fields.php';
 require_once 'util/login-or-create-user.php';
 require_once 'util/page-templates.php';
 
 require_once plugin_dir_path( __FILE__ ) . 'util/dashboard.php';
 
-if ( is_admin() ) {
-  require_once plugin_dir_path( __FILE__ ) . 'admin/dashboard.php';
-  require_once plugin_dir_path( __FILE__ ) . 'admin/plugin-activation.php';
-}
-
-
 add_action( 'init', function() {
+  if ( !class_exists( 'Timber' ) ) {
+    return;
+  }
+
   $results = scandir(WP_PLUGIN_DIR);
 
   $locations = [];
