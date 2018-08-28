c05274a48167cf1e05ea9a70324527af4e1ef1c8
Jascha Ehrenreich <jascha@jaeh.at>
2018-08-28 16:13:06 +0200

    add admin styles

--- includes/admin/index.php
+++ includes/admin/index.php
@@ -2,3 +2,4 @@
 require_once 'dashboard.php';
 require_once 'plugin-activation.php';
 require_once 'requirements.php';
+require_once 'styles/index.php';
---
+++ includes/admin/styles/index.php
@@ -0,0 +1,9 @@
+<?php
+
+add_action( 'admin_head', function() {
+  echo "<style>
+  .magic-input {
+    width: 100%;
+  }
+</style>";
+} );
