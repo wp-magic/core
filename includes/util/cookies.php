08e7fedc3f64d77e4ff69ddd4ed4d7cc7f0a2cdb
Jascha <jascha@jaeh.at>
2019-11-06 21:57:47 +0100

    format, do not use $_POST

--- includes/util/cookies.php
+++ includes/util/cookies.php
@@ -1,35 +1,50 @@
 <?php
 
+/**
+ * Deserialize a cookie string into an array.
+ *
+ * @param string $str input string to derive from.
+ * @param string $sep separator string to split by.
+ *
+ * @return array array of cookie key => value pairs.
+ */
 function magic_deserialize_cookie( string $str, string $sep = MAGIC_DASHBOARD_COOKIE_SEP ) {
-  $string_array = explode( PHP_EOL, $str);
-  $cookies = [];
-  foreach ( $string_array as $string ) {
-    $arr = explode( $sep, $string );
+	$string_array = explode( PHP_EOL, $str );
+	$cookies      = [];
 
-    if ( empty( $arr ) || empty( $arr[0] ) ) {
-      break;
-    }
+	foreach ( $string_array as $string ) {
+		$arr = explode( $sep, $string );
 
-    $name = esc_html( trim( $arr[0] ) );
-    $slug = esc_html( trim( $arr[1] ) );
-    $desc = esc_html( trim( $arr[2] ) );
-    $cook = esc_html( trim( $arr[3] ) );
-    $on   = !empty($_POST[$slug]) || !empty( $_POST['accept_all'] );
+		if ( empty( $arr ) || empty( $arr[0] ) ) {
+			break;
+		}
 
-    $cookies[] = array (
-      'name' => $name,
-      'slug' => $slug,
-      'description' => $desc,
-      'cookies' => explode( ',', $cook ),
-      'on' => $on,
-    );
-  }
+		$name = esc_html( trim( $arr[0] ) );
+		$slug = esc_html( trim( $arr[1] ) );
+		$desc = esc_html( trim( $arr[2] ) );
+		$cook = esc_html( trim( $arr[3] ) );
 
-  return $cookies;
+		$cookies[] = array(
+			'name'        => $name,
+			'slug'        => $slug,
+			'description' => $desc,
+			'cookies'     => explode( ',', $cook ),
+		);
+	}
+
+	return $cookies;
 }
 
+/**
+ * Serialize a cookie array into a string.
+ *
+ * @param array  $array input array to concatenate.
+ * @param string $sep separator to join by.
+ *
+ * @return string a serialized cookie.
+ */
 function magic_serialize_cookie( array $array, string $sep = MAGIC_DASHBOARD_COOKIE_SEP ) {
-  $array = implode( $sep, $array );
-  $string = implode( PHP_EOL, $array );
-  return $string;
+	$array  = implode( $sep, $array );
+	$string = implode( PHP_EOL, $array );
+	return $string;
 }
